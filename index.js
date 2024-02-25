2c35a579826f947ea7b10fd128073ae250ad89af
zuber57 <zubarahmad48@gmail.com>
2024-02-25 11:10:42 +0530

    -

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-$('.slider_cad').slick({
+$('.cad_slick').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    arrows:false,
+    pagination:false,
+    loop:true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -18,6 +22,7 @@ $('.slider_cad').slick({
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
+          dots: false,
           slidesToScroll: 2
         }
       },
@@ -25,11 +30,10 @@ $('.slider_cad').slick({
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
+          dots: false,
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+    
     ]
   });
